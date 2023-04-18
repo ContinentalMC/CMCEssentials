6601e6a119ed3340c67b1015efcd949f4eb1bd8d
Kyle <kpp0620@gmail.com>
2023-04-17 23:43:33 -0400

    Adding emojis and fixed error with welcome back : DONE

--- src/main/java/com/yopal/continentalmc/commands/CMCCommand.java
+++ src/main/java/com/yopal/continentalmc/commands/CMCCommand.java
@@ -33,8 +33,17 @@ public class CMCCommand implements CommandExecutor {
             ConfigManager.reloadConfig(cmc);
             EmojiManager.reloadFile(cmc);
             PlayerInteract.sendMessage(player, ChatColor.GREEN + "Files successfully reloaded!");
-        } else {
+        } else if (args[0].equalsIgnoreCase("reload") && !player.hasPermission("cmc.admin.reload")) {
             PlayerInteract.sendLackPermission(player, "cmc.admin.reload");
+        }
+
+        if (args[0].equalsIgnoreCase("emojis") && player.hasPermission("cmc.user.emojisList")) {
+            PlayerInteract.sendMessage(player, ChatColor.GREEN + "Here are all the emojis!");
+            for (String string : EmojiManager.getEmojis().keySet()) {
+                player.sendMessage(ChatColor.GRAY + "  - " + string + " = " + EmojiManager.getEmojis().get(string));
+            }
+        } else if (args[0].equalsIgnoreCase("emojis") && !player.hasPermission("cmc.user.emojisList")) {
+            PlayerInteract.sendLackPermission(player, "cmc.user.emojisList");
         }
 
 
--- src/main/java/com/yopal/continentalmc/listeners/PlayerEmojiListener.java
+++ src/main/java/com/yopal/continentalmc/listeners/PlayerEmojiListener.java
@@ -8,6 +8,7 @@ import com.yopal.continentalmc.utils.PlayerInteract;
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
